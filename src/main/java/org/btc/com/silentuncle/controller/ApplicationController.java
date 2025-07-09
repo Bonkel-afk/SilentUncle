8f9a647396f8a5570e20d1e9b57ebfedfd2edba8
JasonVoigt <Zockeryolo123@gmail.com>
2025-07-09 11:06:33 +0200

    Added Better Design to Main Page -> TODO: Make App Work

--- src/main/java/org/btc/com/silentuncle/controller/ApplicationController.java
+++ src/main/java/org/btc/com/silentuncle/controller/ApplicationController.java
@@ -8,7 +8,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import org.btc.com.silentuncle.ResourceBundleContainer;
 import org.btc.com.silentuncle.events.MainPageEvent;
@@ -17,16 +16,13 @@ import org.btc.com.silentuncle.events.TrayIconEvent;
 import org.btc.com.silentuncle.services.LocaleService;
 import org.btc.com.silentuncle.view.TrayIconView;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.MessageSource;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.Locale;
 import java.util.Objects;
-import java.util.ResourceBundle;
 
 
 @SuppressWarnings("CallToPrintStackTrace")
@@ -42,24 +38,16 @@ public class ApplicationController {
     private ComboBox<String> languageComboBox;
 
     @FXML
-    private ImageView logoImageView;
-
-    @FXML
     private Label userLabel;
 
     @FXML
     private Button settingsButton;
 
-    @FXML
-    private Button tierAlarmButton;
-
-    @FXML
-    private Button feuerAlarmButton;
-
     @Autowired
     private ResourceBundleContainer resourceBundleContainer;
     @Autowired
     private LocaleService localeService;
+
 
 
     ApplicationController(ApplicationContext applicationContext, TrayIconView trayIconView) {
@@ -108,11 +96,6 @@ public class ApplicationController {
     private void handleFeuerAlarm() {
         System.out.println("Feuer Alarm ausgelöst!");
         trayIconView.displayFireAlarm();
-    }
-
-    @FXML
-    private void handleSettings() {
-        System.out.println("Einstellungen geöffnet.");
     }
 
     @EventListener
@@ -188,6 +171,5 @@ public class ApplicationController {
         stage.show();
         return stage;
     }
-
 
 }
